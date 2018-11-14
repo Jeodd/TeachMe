397944ffcd43a4811d3f0daf1c1c91742d7f2006
jmartinelli <jacques.sefas@gmail.com>
2018-11-14 16:45:07 +0100

    First commit : architecture set up and build configuration

---
+++ src/main/java/fr/utbm/TeachMe/models/Clients.java
@@ -0,0 +1,10 @@
+package fr.utbm.TeachMe.models;
+
+import java.io.Serializable;
+
+public class Clients implements Serializable {
+    Integer clientId;
+    String clientLastname;
+    String clientLastnameFirstname;
+    String Address;
+}
